412ae76f8d7443416f0df57dda5c300a89b74a4d
Oggup <edifoo@gmail.com>
2022-06-23 15:59:10 -0700

    game resets when a player reaches 5 points

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let options = ["rock", "paper", "scissors"];
 let playerOptions = document.querySelectorAll(".btn-list");
 const winners = [];
 let round = 1;
-console.log(playerOptions);
+// console.log(playerOptions);
 //what i seek to accomplish
 //on start game click options for rock paper and scissors buttons appear on the screen and start game button is removed from
 //whenever a option button is clicked add small transition to emphasize button clicked
@@ -18,7 +18,7 @@ playerOptions.forEach((option) => {
     playRound(round, input);
     logWins();
     round++;
-    console.log(input);
+    // console.log(input);
   });
 });
 
@@ -28,7 +28,7 @@ function playRound(round, input) {
   const computerSelection = computerChoice();
   const winner = compareSelections(playerSelection, computerSelection);
   winners.push(winner);
-  console.log("winner of the round:", winner);
+  // console.log("winner of the round:", winner);
   logRounds(playerSelection, computerSelection, winner, round);
 }
 
@@ -54,26 +54,42 @@ function compareSelections(playerSelection, computerSelection) {
   }
 }
 
+function restartGame(){
+  round=1
+  document.getElementById("round-number").innerText = round
+  document.getElementById("player-selection").innerText ="Selection";
+  // console.log("Computer chose", computerChoice);
+  document.getElementById("computer-selection").innerText = "Selection";
+}
+
 function logWins() {
   let playerWins = winners.filter((item) => item == "PLAYER").length;
   let computerWins = winners.filter((item) => item == "COMPUTER").length;
   let ties = winners.filter((item) => item == "TIE").length;
-  console.log("Results:");
-  console.log("player wins:", playerWins);
+  // console.log("Results:");
+  // console.log("player wins:", playerWins);
   document.getElementById("player-score").innerText = playerWins;
-  console.log("computer wins:", computerWins);
+  // console.log("computer wins:", computerWins);
   document.getElementById("computer-score").innerText = computerWins;
-  console.log("ties:", ties);
-  console.log(winners);
+  // console.log("ties:", ties);
+  // console.log(winners)
+  if (playerWins===4){
+    console.log("YOU WON FIVE TIMES")
+    restartGame()
+  }
+  if(computerWins===4){
+    console.log("Jotchua won the game")
+    restartGame()
+  }
 }
 
 function logRounds(playerChoice, computerChoice, winner, round) {
   document.getElementById("round-number").innerText = round;
-  console.log("Round:", round);
-  console.log("Player chose:", playerChoice);
+  // console.log("Round:", round);
+  // console.log("Player chose:", playerChoice);
   document.getElementById("player-selection").innerText = playerChoice;
-  console.log("Computer chose", computerChoice);
+  // console.log("Computer chose", computerChoice);
   document.getElementById("computer-selection").innerText = computerChoice;
-  console.log(winner, "won the round");
-  console.log("-------------------------");
+  // console.log(winner, "won the round");
+  // console.log("-------------------------");
 }
